ab9e23667abdfc347238a13591db15123daf898b
nanako45a <nsz.rnmk120@gmail.com>
2023-08-25 01:36:20 +0900

    ‘編集、削除機能を実装しました’

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index new create show] do
+  resources :boards, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create edit destroy], shallow: true
   end
 
