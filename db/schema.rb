f2f9662a22d1bef06200c414f4464fcf567d0345
nanako45a <nsz.rnmk120@gmail.com>
2023-09-22 23:26:20 +0900

    ‘rails基礎20重複したカラムを削除しました’

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_134838) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_me_token"
@@ -62,12 +64,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_134838) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
-    t.string "first_name"
-    t.string "last_name"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "boards", "users"
